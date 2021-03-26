95cdb841517dd017008e9f3ca26df03f633b2500
Vishwajyoti Pandey <ext_vishwajyotip@falabella.cl>
2021-03-26 23:01:29 +0530

    Changing the order of parameter passed

--- polyaxon_dashboard.sh
+++ polyaxon_dashboard.sh
@@ -2,9 +2,9 @@
 #Install polyaxon python library
 sudo pip3 install polyaxon
 #Name of the K8s cluster installed for polyaxon
-CLUSTER_NAME = $1
+CLUSTER_NAME = $2
 #Computaion Zone under which K8s cluster is installed
-ZONE = $2
+ZONE = $1
 gcloud container clusters get-credentials $CLUSTER_NAME --zone $ZONE
 polyaxon config set --host=http://localhost:8000
 kubectl port-forward -n polyaxon svc/polyaxon-polyaxon-gateway 8000:80
